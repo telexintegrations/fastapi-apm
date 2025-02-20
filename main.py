e2158d01850f75318569feab0eb51fd4971cb3bd
Wasiu Bakare <wasiubakare@gmail.com>
2025-02-20 11:41:44 +0100

    chore: setup for prod

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI
 from apm.error_handler import global_exception_handler
 from api.routes import router
 import logging
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.add_exception_handler(Exception, global_exception_handler)
 
